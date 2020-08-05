4b9066b1925155b7d41060047eec724b547d1f1b
fsluizvictor <fs.luizvictor@gmail.com>
2020-08-05 15:09:09 -0300

    configurando as rotas do sistema

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
@@ -22,15 +23,15 @@ function Landing() {
                 <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="study" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="" className="study">
+                    <Link to="give-classes" className="study">
                         <img src={giveClassesIcon} alt="Estudar" />
                         Dar Aulas
-                    </a>
+                    </Link>
 
                 </div>
 
