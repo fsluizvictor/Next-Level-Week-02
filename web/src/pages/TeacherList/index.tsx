a90f35f44a679e6313a85771819368450df2bee8
fsluizvictor <fs.luizvictor@gmail.com>
2020-08-05 17:24:30 -0300

    acrescentando formulário

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,37 @@
-import React from  'react'
+import React from 'react'
+import PageHeader from '../../components/PageHeader'
+import './styles.css'
 
-function TeacherList(){
-    return(
-        <h1>Teacher</h1>
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">
+                            Matéria
+                        </label>
+                        <input type="text" id="subject" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="week_day">
+                            Dia da semana
+                        </label>
+                        <input type="text" id="subject" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">
+                            Horário
+                        </label>
+                        <input type="text" id="subject" />
+                    </div>
+                </form>
+
+            </PageHeader>
+        </div >
     )
 }
 
