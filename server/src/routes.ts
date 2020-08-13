ba1c9fb5bef34df0fc7d38db9cd3e15a3fe42094
fsluizvictor <fs.luizvictor@gmail.com>
2020-08-12 23:51:10 -0300

    criando o controle das classe Classes

---
+++ server/src/routes.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import ClassesController from './database/controllers/ClassesController'
+
+const routes = express.Router()
+
+const classesController = new ClassesController()
+
+routes.post('/classes', classesController.create)
+routes.get('/classes', classesController.index)
+
+
+export default routes
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
 app.use(express.json())
 
-app.listen(3333)
+app.use(routes)
+
+app.listen(3333, () => { console.log('funfou') })
